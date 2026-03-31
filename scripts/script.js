922872f7c120aed01632352479893242394e0aa0
MarcoHelgeDev <yoshi-182@web.de>
2026-03-31 16:18:11 +0200

    add: cache

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,59 @@
 const pkmnContainer = document.querySelector(".js-pkmn-render-container");
 const pkmnSearchBtn = document.querySelector(".js-pkmn-search-btn");
+const pkmnLoadBtn = document.querySelector(".js-pkmn-load-btn");
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+let pkmnData = [];
+let pkmnCache = {};
+let startIndex = 0;
+let endIndex = 20;
 
-const getPokemon = async function (requestedPkmn) {
-  if (!requestedPkmn) {
-    let limit = "?limit=1025";
-    let response = await fetch(BASE_URL + limit);
-    let pkmnData = await response.json();
-    return pkmnData.results;
+const getPokemon = async function () {
+  let limit = "?limit=1025";
+  let response = await fetch(BASE_URL + limit);
+  let result = await response.json();
+  return result.results;
+};
+
+const renderPkmn = async function () {
+  startLoadingScreen();
+  pkmnLoadBtn.disabled = true;
+  let currentEnd = Math.min(pkmnData.length, endIndex);
+  let html = "";
+
+  if (startIndex >= pkmnData.length) {
+    endLoadingScreen();
+    return;
+  }
+
+  for (startIndex; startIndex < currentEnd; startIndex++) {
+    let pkmnURL = pkmnData[startIndex].url;
+    let pokemonData;
+
+    if (pkmnCache[pkmnURL] !== undefined) {
+      pokemonData = pkmnCache[pkmnURL];
+    } else {
+      let response = await fetch(pkmnURL);
+      let result = await response.json();
+      pkmnCache[pkmnURL] = result;
+      pokemonData = result;
+    }
+
+    html += getPkmnTemplate(pokemonData);
+  }
+  pkmnContainer.innerHTML += html;
+  endIndex += 20;
+  endLoadingScreen();
+  if (startIndex >= pkmnData.length) {
+    pkmnLoadBtn.disabled = true;
   } else {
-    let response = await fetch(BASE_URL + requestedPkmn);
-    let result = await response.json();
-    return renderRequest(result);
+    pkmnLoadBtn.disabled = false;
   }
 };
 
 const renderRequest = async function (pkmnData) {
   let html = "";
-  console.log(pkmnData);
-
   html += getPkmnTemplate(pkmnData);
-  pkmnContainer.innerHTML = html;
-};
-
-const renderPkmn = async function () {
-  let data = await getPokemon();
-  let html = "";
-
-  for (let i = 0; i <= 20; i++) {
-    let pkmnURL = data[i].url;
-    let response = await fetch(pkmnURL);
-    let result = await response.json();
-    html += getPkmnTemplate(result);
-  }
   pkmnContainer.innerHTML = html;
 };
 
@@ -58,8 +78,21 @@ pkmnSearchBtn.addEventListener("click", function () {
   getPokemon(inputField);
 });
 
-const init = function () {
-  renderPkmn();
+pkmnLoadBtn.addEventListener("click", renderPkmn);
+
+const startLoadingScreen = function () {
+  const spinner = document.querySelector(".js-spinner-container");
+  spinner.classList.remove("hidden");
+};
+
+const endLoadingScreen = function () {
+  const spinner = document.querySelector(".js-spinner-container");
+  spinner.classList.add("hidden");
+};
+
+const init = async function () {
+  pkmnData = await getPokemon();
+  await renderPkmn();
 };
 
 init();
